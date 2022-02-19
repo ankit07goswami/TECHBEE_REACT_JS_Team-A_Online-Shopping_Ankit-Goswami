eec91aef6cb88fc3c424ee1fc9a3b0dd9d0587fc
Ankit Goswami <98594004+ankit07goswami@users.noreply.github.com>
2022-02-19 19:40:14 +0530

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,124 @@
+const products = [
+  {
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "iPhone 11 Pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: "Sony Playstation 4 Pro White Version",
+    image: "/images/playstation.jpg",
+    description:
+      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    brand: "Sony",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: "Sony Playstation 5",
+    image: "/images/playstation5.jpg",
+    description:
+      "Explore uncharted virtual territories and slay dragons with this sleek Sony PlayStation 5 gaming console. The 825GB SSD allows ultra-fast load times, while 3-D audio output produces crisp acoustics. This Sony PlayStation 5 gaming console supports haptic feedback for effortless communication in multiplayer setups, and adaptive triggers allow immersive gameplay",
+    brand: "Sony",
+    category: "Electronics",
+    price: 499.99,
+    countInStock: 15,
+    rating: 5,
+    numReviews: 5,
+  },
+  {
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: "Amazon Echo Dot 3rd Generation",
+    image: "/images/alexa.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: "Amazon Kindle Paperwhite",
+    image: "/images/kindle_paperwhite.jpg",
+    description:
+      "Amazon's thinnest, lightest Kindle Paperwhite yet, with a sleek, modern design so you can read comfortably for hours. Features our signature 300 ppi, glare-free Paperwhite display, printed-quality text and twice the storage of the previous generation. Plus a single battery charge lasts weeks, not hours",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 119.99,
+    countInStock: 8,
+    rating: 4,
+    numReviews: 6,
+  },
+  {
+    name: "Garmin vivofit 4 Activity Tracker",
+    image: "/images/garmin_vivofit.jpg",
+    description:
+      "Garmin vivofit 4 is a slim, swim-friendly activity tracker with a customisable colour display and 1+ year battery life. Track your steps, sleep and more, plus get a personalised daily step goal based on your habits. With the Move IQ feature, vivofit 4 automatically captures a variety of activities, such as walking, running, biking and more, to view on Garmin Connect",
+    brand: "Garmin",
+    category: "Electronics",
+    price: 79.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 1,
+  },
+  {
+    name: "Apple iPad Pro 2020 11 Inch",
+    image: "/images/ipad.jpg",
+    description:
+      "Your next computer is not a computer. It's a magical piece of glass. It's so fast most PC laptops can't keep up. It has pro cameras that can transform reality. And you can use it with touch, pencil, keyboard, and now trackpad. It's the new iPad Pro",
+    brand: "Apple",
+    category: "Electronics",
+    price: 799.99,
+    countInStock: 9,
+    rating: 5,
+    numReviews: 8,
+  },
+];
+
+export default products;
